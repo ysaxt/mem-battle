66bfa03dba679cc729e142ecec770df5129ad627
ysaxt <20maxter07@gmail.com>
2026-09-03 01:01:37 +0300

    Update supabase-config.js

--- supabase-config.js
+++ supabase-config.js
@@ -1,5 +1,5 @@
 // ВСТАВЬТЕ СЮДА данные проекта Supabase.
 // Project URL: Settings -> API -> Project URL
 // Anon key: Settings -> API -> anon/public key
-window.SUPABASE_URL = 'https://glhdtcqiifekjkgtynhx.supabase.co/rest/v1/';
+window.SUPABASE_URL = 'https://glhdtcqiifekjkgtynhx.supabase.co';
 window.SUPABASE_ANON_KEY = 'sb_publishable_g9rSPEHvW8z7oeWJMXLfgg_5uKYRRS9';
